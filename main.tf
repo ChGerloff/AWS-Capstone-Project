45fba1fcac2942b3614e34f3c10de63baf907555
Chris <christoph.gerloff@gmx.de>
2026-02-04 17:37:08 +0100

    added security group and webserver instance

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "web_sg" {
 }
 
 resource "aws_instance" "web_server" {
-  ami                    = "ami-0c02fb55956c7d316"
+  ami                    = "ami-0c94855ba95b798c7"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
