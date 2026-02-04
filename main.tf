678ae0d3e86d671e592b3403ce911045ab6cfe4d
Chris <christoph.gerloff@gmx.de>
2026-02-04 17:50:13 +0100

    added igw

--- main.tf
+++ main.tf
@@ -21,10 +21,41 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.dev_vpc.id
   cidr_block              = var.subnet_cidr
   map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
 
   tags = {
     Name = "test-public-subnet"
   }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_security_group" "web_sg" {
