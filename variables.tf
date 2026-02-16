8d0e652f3e6507bb048e52f3e046b3425266a287
Chris <christoph.gerloff@gmx.de>
2026-02-16 14:00:55 +0100

    updated teraform architecture

--- variables.tf
+++ variables.tf
@@ -10,8 +10,25 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "subnet_cidr" {
-  description = "CIDR block for the subnet"
+variable "public_subnet_cidrs" {
+  description = "CIDRs for public subnets in 2 AZs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDRs for private subnets in 2 AZs"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for WordPress"
   type        = string
-  default     = "10.0.1.0/24"
+  default     = "t3.small"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
 }
