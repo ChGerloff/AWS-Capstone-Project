cb7b60477a9fcb1f993644ffb3a6ff30574880d6
Chris <christoph.gerloff@gmx.de>
2026-02-16 14:01:08 +0100

    updated teraform architecture

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,26 @@ output "vpc_id" {
   value = aws_vpc.dev_vpc.id
 }
 
-output "subnet_id" {
-  value = aws_subnet.public.id
+output "public_subnet_a_id" {
+  value = aws_subnet.public_a.id
+}
+
+output "public_subnet_b_id" {
+  value = aws_subnet.public_b.id
+}
+
+output "private_subnet_a_id" {
+  value = aws_subnet.private_a.id
+}
+
+output "private_subnet_b_id" {
+  value = aws_subnet.private_b.id
+}
+
+output "web_server_public_ip" {
+  value = aws_instance.web_server.public_ip
+}
+
+output "web_server_public_dns" {
+  value = aws_instance.web_server.public_dns
 }
