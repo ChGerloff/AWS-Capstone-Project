091d3d101671523629b1147ad29928bedd44bb32
Chris <christoph.gerloff@gmx.de>
2026-02-25 15:42:18 +0100

    Changed ingress rule in security groups

--- main.tf
+++ main.tf
@@ -111,12 +111,11 @@ resource "aws_security_group" "web_sg" {
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.web_sg.id
 #  cidr_ipv4         = aws_vpc.main.cidr_block
-    from_port   = 80
-    to_port     = 80
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 }
-
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.web_sg.id
 #  cidr_ipv4         = aws_vpc.main.cidr_block
@@ -126,14 +125,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
     cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.web_sg.id
-#  cidr_ipv4         = aws_vpc.main.cidr_block
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-}
+
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http2" {
   security_group_id = aws_security_group.web_sg.id
