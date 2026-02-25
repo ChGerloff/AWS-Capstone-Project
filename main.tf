5290f391fc00c48db266f8115bc1b7c225591a77
Chris <christoph.gerloff@gmx.de>
2026-02-25 15:39:56 +0100

    Change ingress/egress rules in security groups

--- main.tf
+++ main.tf
@@ -98,41 +98,61 @@ resource "aws_subnet" "private_b" {
 }
 
 resource "aws_security_group" "web_sg" {
-  name_prefix = "web-sg"
-  vpc_id      = aws_vpc.dev_vpc.id
+  name        = "web-sg"
+  description = "Allow inbound traffic and outbound traffic"
+  vpc_id      = aws_vpc.main.id
 
-  ingress {
+  tags = {
+    Name = "web_sg"
+  }
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.web_sg.id
+#  cidr_ipv4         = aws_vpc.main.cidr_block
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+}
 
-  ingress {
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.web_sg.id
+#  cidr_ipv4         = aws_vpc.main.cidr_block
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.web_sg.id
+#  cidr_ipv4         = aws_vpc.main.cidr_block
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+}
 
-  ingress {
+resource "aws_vpc_security_group_ingress_rule" "allow_http2" {
+  security_group_id = aws_security_group.web_sg.id
+#  cidr_ipv4         = aws_vpc.main.cidr_block
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+}
 
-  egress {
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.web_sg.id
+#  cidr_ipv4         = aws_vpc.main.cidr_block
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web-sg"
-  }
 }
+
 
 # IAM Role for EC2 instance to access S3
 /* Removed IAM role, role policy and instance profile resources.
