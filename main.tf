dad91bb7d289c5f74240c872d12e4063f17ff5d5
Chris <christoph.gerloff@gmx.de>
2026-02-25 18:15:00 +0100

    Fixed egress

--- main.tf
+++ main.tf
@@ -139,9 +139,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http2" {
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.web_sg.id
-#  cidr_ipv4         = aws_vpc.main.cidr_block
-    from_port   = 0
-    to_port     = 0
     ip_protocol    = "-1"
     cidr_ipv4 = "0.0.0.0/0"
 }
